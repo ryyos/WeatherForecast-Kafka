7cda83878fc61af14ef224f19b6d0fc34c142b01
ryosyntaxx <riodwi12174@gmail.com>
2023-12-16 21:42:30 +0700

    add argparse with defaulth value

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from libs import Parser
 from libs import Consumer
 from libs import Producer
@@ -16,23 +17,28 @@ class Main:
         self.__kafka_produser = Producer(server=server)
 
 
-    def ex(self):
+    def main(self):
         try:
-            while True:
-                for message in self.__kafka_consumer.accept():
-                    results: Response = self.__parser.fetch_data(location=message.value.decode("utf-8"))
-
-                    if results != 404:
-                        self.__writer.ex(path=f'{self.__path}/{message.value.decode("utf-8")}.json', content=results.json())
-                        self.__logs.info(requests=message.value.decode("utf-8"), status=results.status_code, offset=message.offset, response=results.json())
+            for message in self.__kafka_consumer.accept():
+                results: Response = self.__parser.fetch_data(location=message.value.decode("utf-8"))
+                if results != 404:
+                    self.__writer.ex(path=f'{self.__path}/{message.value.decode("utf-8")}.json', content=results.json())
+                    self.__logs.info(requests=message.value.decode("utf-8"), status=results.status_code, offset=message.offset, response=results.json())
                     
 
         except KeyboardInterrupt:
             ic("stoped")
 
 
+
 if __name__ == "__main__":
-    main = Main(server="127.0.0.1:9092", topic="city", path="data")
-    main.ex()
+    request = argparse.ArgumentParser(description='Weatherstack scraping and distributed with kafka')
+    request.add_argument('--server', '--s', type=str, default='127.0.0.1:9092', help='Enter your Kafka server[defaulth: 127.0.0.1:9092]')
+    request.add_argument('--topic', '--t', type=str, default='weather', help='Enter your Kafka topic')
+    request.add_argument('--path', '--p', type=str, default='data', help='Enter the path if you want to save the scraping results locally')
+    request = request.parse_args()
+
+    main = Main(server=request.server , topic=request.topic, path=request.path)
+    main.main()
 
 
