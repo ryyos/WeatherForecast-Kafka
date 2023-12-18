3ce24d94d2afd0e2c191181ca7cc83f9e1248b2f
ryosyntaxx <riodwi12174@gmail.com>
2023-12-18 15:52:58 +0700

    add argparser in elactic consumer

--- elastic_consumer.py
+++ elastic_consumer.py
@@ -1,4 +1,4 @@
-import uuid
+import argparse
 import json
 from elasticsearch import Elasticsearch
 from icecream import ic
@@ -6,8 +6,8 @@ from libs import Logs
 from kafka import KafkaConsumer
 
 class Elastic:
-    def __init__(self, topic: str, server_k: str) -> None:
-        self.__elasticsearch = Elasticsearch(['http://192.168.20.90:9200'])
+    def __init__(self, topic: str, server_k: str, server_el: str) -> None:
+        self.__elasticsearch = Elasticsearch([server_el])
         self.__logs = Logs()
         self.__kafka_consumer = KafkaConsumer(topic, bootstrap_servers= server_k)
         
@@ -23,6 +23,13 @@ class Elastic:
             ic('Stopped')
 
 if __name__ == '__main__':
-    es = Elastic(topic='elastic', server_k='127.0.0.1:9092')
-    es.send(index='weather_tomorrow')
+    request = argparse.ArgumentParser()
+    request.add_argument('--topic', '--t', type=str, default='elastic')
+    request.add_argument('--server_k', '--sk', type=str, default='127.0.0.1:9092')
+    request.add_argument('--server_el', '--sel', type=str, default='http://192.168.20.90:9200')
+    request.add_argument('--index', '--i', type=str, default='weather_tomorrow')
+    request = request.parse_args()
+
+    es = Elastic(topic=request.topic , server_k=request.server_k, server_el=request.server_el)
+    es.send(index=request.index)
 
